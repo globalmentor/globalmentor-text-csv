b0f304e526120d3ec316b40c5a17c909c2a81031
Garret Wilson <garret@globalmentor.com>
2006-06-07 00:39:10 +0000

    Added new logging system. Created workaround for IE6 caching bug.

--- src/com/garretwilson/csv/CSVConstants.java
+++ src/com/garretwilson/csv/CSVConstants.java
@@ -1,19 +1,22 @@
 package com.garretwilson.csv;
 
-import com.garretwilson.lang.StringUtilities;
-import com.garretwilson.text.CharacterConstants;
+import static com.garretwilson.text.CharacterConstants.*;
 
 /**Constant values for Comma Separated Value (CSV) files.
 @author Garret Wilson
+@see <a href="http://www.rfc-editor.org/rfc/rfc4180.txt">RFC 4180: Common Format and MIME Type for Comma-Separated Values (CSV) Files</a>
 */
-public interface CSVConstants
+public class CSVConstants
 {
 	/**The character that delimits CSV fields.*/
-	public final static char FIELD_DELIMITER_CHAR=CharacterConstants.COMMA_CHAR;
+	public final static char FIELD_DELIMITER_CHAR=COMMA_CHAR;
 
-	/**The character that delimits records.*/
-	public final static char RECORD_DELIMITER_CHAR=CharacterConstants.LINE_FEED_CHAR;
+	/**The string that delimits records.*/
+	public final static String RECORD_DELIMITER_STRING=""+CARRIAGE_RETURN_CHAR+LINE_FEED_CHAR;
+
+	/**Characters that require a field to be quoted.*/
+	public final static String RESTRICTED_CHARACTERS=RECORD_DELIMITER_STRING+FIELD_DELIMITER_CHAR+QUOTATION_MARK_CHAR;
 
 	/**A quote character that has been escapted.*/
-	public final static String ESCAPED_QUOTATION_MARK_STRING=StringUtilities.createString(CharacterConstants.QUOTATION_MARK_CHAR, 2);
+	public final static String ESCAPED_QUOTATION_MARK_STRING=""+QUOTATION_MARK_CHAR+QUOTATION_MARK_CHAR;
 }
--- src/com/garretwilson/csv/CSVSerializer.java
+++ src/com/garretwilson/csv/CSVSerializer.java
@@ -1,51 +1,23 @@
 package com.garretwilson.csv;
 
 import java.io.*;
-import com.garretwilson.lang.StringBufferUtilities;
 
+import static com.garretwilson.csv.CSVConstants.*;
+import static com.garretwilson.lang.CharSequenceUtilities.*;
+import static com.garretwilson.lang.StringBuilderUtilities.*;
 import static com.garretwilson.text.CharacterConstants.*;
 
 /**Serializes Comma Separated Value (CSV) information
 @author Garret Wilson
 */
-public class CSVSerializer implements CSVConstants
+public class CSVSerializer
 {
-
-	/**Serializes the given objects to the provided output stream using the
-	UTF-8 encoding scheme.
-	@param outputStream The output stream to which the objects will be serialized.
-	@param objects The objects to serialize, each of which may be
-		<code>null</code>.
-	@exception IOException if there is an error writing the CSV record.
-	*/
-/*G****del; this won't work with multiple calls.
-	public static void serialize(final OutputStream outputStream, final Object[] objects) throws IOException
-	{
-		serialize(outputStream, objects, CharacterEncodingConstants.UTF_8);	//serialize using UTF-8
-	}
-*/
-
-	/**Serializes the given objects to the provided output stream.
-	@param outputStream The output stream to which the objects will be serialized.
-	@param objects The objects to serialize, each of which may be
-		<code>null</code>.
-	@param encoding The character encoding.
-	@exception IOException if there is an error writing the CSV record.
-	*/
-/*G****del; this won't work with multiple calls.
-	public static void serialize(final OutputStream outputStream, final Object[] objects, final String encoding) throws IOException
-	{
-		serialize(new OutputStreamWriter(outputStream, encoding), objects);	//serialize using the given encoding
-	}
-*/
-
 	/**Serializes the given objects to the provided writer.
 	@param writer The writer to which the objects will be serialized.
-	@param objects The objects to serialize, each of which may be
-		<code>null</code>.
+	@param objects The objects to serialize, each of which may be <code>null</code>.
 	@exception IOException if there is an error writing the CSV record.
 	*/
-	public static void serialize(final Writer writer, final Object[] objects) throws IOException
+	public static void serialize(final Writer writer, final Object... objects) throws IOException
 	{
 		for(int i=0; i<objects.length; ++i)	//look at each object
 		{
@@ -60,24 +32,28 @@ public class CSVSerializer implements CSVConstants
 				writer.write(field);	//write the field
 			}
 		}
-		writer.write(RECORD_DELIMITER_CHAR);	//write the end of the record
+		writer.write(RECORD_DELIMITER_STRING);	//write the end of the record
 	}
 
 	/**Encodes a string to be represented in a CSV field.
-	Encoding includes:
-	<ul>
-		<li>Escaping internal quotation marks by doubling them.</li>
-		<li>Adding beginning and ending quotation marks.</li>
-	</ul>
-	@param string The string to encode. 
+	If the character sequence includes a comma, double-quote, or a line break (CR or LF),
+	the string is encoded and placed in quotes. Double-quotes are escaped by doubling them.
+	@param charSequence The character sequence to encode. 
 	@return An encoded string appropriate for serialization as a CSV field.
 	 */
-	public static String encodeField(final String string)
+	public static String encodeField(final CharSequence charSequence)
 	{
-		final StringBuffer stringBuffer=new StringBuffer(string);	//create a new string buffer to process the string 
-		StringBufferUtilities.replace(stringBuffer, QUOTATION_MARK_CHAR, ESCAPED_QUOTATION_MARK_STRING);	//replace all quotes with doubled quotes
-		stringBuffer.insert(0, QUOTATION_MARK_CHAR);	//prepend a quotation mark
-		stringBuffer.append(QUOTATION_MARK_CHAR);	//append a quotation mark
-		return stringBuffer.toString();	//return the string we created
+		if(charIndexOf(charSequence, RESTRICTED_CHARACTERS)>=0)	//if a restricted character is in the character sequence
+		{
+			final StringBuilder stringBuilder=new StringBuilder(charSequence);	//create a new string buffer to process the string 
+			replace(stringBuilder, QUOTATION_MARK_CHAR, ESCAPED_QUOTATION_MARK_STRING);	//replace all quotes with doubled quotes
+			stringBuilder.insert(0, QUOTATION_MARK_CHAR);	//prepend a quotation mark
+			stringBuilder.append(QUOTATION_MARK_CHAR);	//append a quotation mark
+			return stringBuilder.toString();	//return the string we created
+		}
+		else	//if there is no restricted character
+		{
+			return charSequence.toString();	//return the character sequence we started with
+		}
 	}
 }
