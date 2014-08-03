6fa88ccd9ba23ba9fffcaa7eab01ba114094d16e
Garret Wilson <garret@globalmentor.com>
2014-08-03 16:04:15 +0000

    Reformatted all source code to more compact form with non-aligned braces.

--- trunk/globalmentor-core/src/main/java/com/globalmentor/text/csv/CSV.java
+++ trunk/globalmentor-core/src/main/java/com/globalmentor/text/csv/CSV.java
@@ -24,75 +24,65 @@ import com.globalmentor.net.ContentType;
 import static com.globalmentor.io.Charsets.*;
 import static com.globalmentor.java.Characters.*;
 
-/**Constant values for Comma Separated Value (CSV) files.
-@author Garret Wilson
-@see <a href="http://www.ietf.org/rfc/rfc4180.txt">RFC 4180: Common Format and MIME Type for Comma-Separated Values (CSV) Files</a>
-*/
-public class CSV
-{
+/**
+ * Constant values for Comma Separated Value (CSV) files.
+ * @author Garret Wilson
+ * @see <a href="http://www.ietf.org/rfc/rfc4180.txt">RFC 4180: Common Format and MIME Type for Comma-Separated Values (CSV) Files</a>
+ */
+public class CSV {
 
-	/**CSV subtype.*/
-	public final static String CSV_SUBTYPE="csv";
+	/** CSV subtype. */
+	public final static String CSV_SUBTYPE = "csv";
 
-	/**The content type for CSV: <code>text/csv</code>.*/
-	public static final ContentType CSV_CONTENT_TYPE=ContentType.create(ContentType.TEXT_PRIMARY_TYPE, CSV_SUBTYPE);
+	/** The content type for CSV: <code>text/csv</code>. */
+	public static final ContentType CSV_CONTENT_TYPE = ContentType.create(ContentType.TEXT_PRIMARY_TYPE, CSV_SUBTYPE);
 
-	/**The name extension for Comma Separated Value (CSV) files.*/
-	public final static String CSV_NAME_EXTENSION="csv";
-	
-	/**The character that delimits CSV fields.*/
-	public final static char FIELD_DELIMITER_CHAR=COMMA_CHAR;
+	/** The name extension for Comma Separated Value (CSV) files. */
+	public final static String CSV_NAME_EXTENSION = "csv";
 
-	/**The string that delimits records.*/
-	public final static String RECORD_DELIMITER_STRING=""+CARRIAGE_RETURN_CHAR+LINE_FEED_CHAR;
+	/** The character that delimits CSV fields. */
+	public final static char FIELD_DELIMITER_CHAR = COMMA_CHAR;
 
-	/**Characters that require a field to be quoted.*/
-	public final static Characters RESTRICTED_CHARACTERS=new Characters(RECORD_DELIMITER_STRING.toCharArray()).add(FIELD_DELIMITER_CHAR, QUOTATION_MARK_CHAR);
+	/** The string that delimits records. */
+	public final static String RECORD_DELIMITER_STRING = "" + CARRIAGE_RETURN_CHAR + LINE_FEED_CHAR;
 
-	/**A quote character that has been escaped.*/
-	public final static String ESCAPED_QUOTATION_MARK_STRING=""+QUOTATION_MARK_CHAR+QUOTATION_MARK_CHAR;
+	/** Characters that require a field to be quoted. */
+	public final static Characters RESTRICTED_CHARACTERS = new Characters(RECORD_DELIMITER_STRING.toCharArray()).add(FIELD_DELIMITER_CHAR, QUOTATION_MARK_CHAR);
 
-	/**Appends a record to the given file with no headers. The information is encoded using UTF-8.
-	@param file The CSV file.
-	@param objects The objects to serialize.
-	@throws IOException if there is an error writing to the file.
-	*/
-	public static void appendRecord(final File file, final Object[] objects) throws IOException
-	{
-		appendRecord(file, null, objects);	//write the objects to the file without headers
+	/** A quote character that has been escaped. */
+	public final static String ESCAPED_QUOTATION_MARK_STRING = "" + QUOTATION_MARK_CHAR + QUOTATION_MARK_CHAR;
+
+	/**
+	 * Appends a record to the given file with no headers. The information is encoded using UTF-8.
+	 * @param file The CSV file.
+	 * @param objects The objects to serialize.
+	 * @throws IOException if there is an error writing to the file.
+	 */
+	public static void appendRecord(final File file, final Object[] objects) throws IOException {
+		appendRecord(file, null, objects); //write the objects to the file without headers
 	}
 
-	/**Appends a record to the given file, first adding the provided headers
-	(if any) if the file does not yet exist. The information is encoded using
-	UTF-8.
-	@param file The CSV file.
-	@param headers The headers with which to initialize the file, or
-		<code>null</code> if no headers are desired.
-	@param objects The objects to serialize, each of which may be
-		<code>null</code>, or <code>null</code> if there are no
-		objects to serialize.
-	@throws IOException if there is an error writing to the file.
-	*/
-	public static void appendRecord(final File file, final String[] headers, final Object[] objects) throws IOException
-	{
-		final boolean exists=file.exists();	//see if the file exists
-			//create a writer to the file, encoding in UTF-8
-		final BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true), UTF_8_CHARSET));
-		try
-		{
-			if(!exists && headers!=null)	//if the file didn't exist and there headers
-			{
-				CSVSerializer.serialize(bufferedWriter, (Object[])headers);	//write the headers
+	/**
+	 * Appends a record to the given file, first adding the provided headers (if any) if the file does not yet exist. The information is encoded using UTF-8.
+	 * @param file The CSV file.
+	 * @param headers The headers with which to initialize the file, or <code>null</code> if no headers are desired.
+	 * @param objects The objects to serialize, each of which may be <code>null</code>, or <code>null</code> if there are no objects to serialize.
+	 * @throws IOException if there is an error writing to the file.
+	 */
+	public static void appendRecord(final File file, final String[] headers, final Object[] objects) throws IOException {
+		final boolean exists = file.exists(); //see if the file exists
+		//create a writer to the file, encoding in UTF-8
+		final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true), UTF_8_CHARSET));
+		try {
+			if(!exists && headers != null) { //if the file didn't exist and there headers
+				CSVSerializer.serialize(bufferedWriter, (Object[])headers); //write the headers
 			}
-			if(objects!=null)	//if there are objects to serialize
-			{
-				CSVSerializer.serialize(bufferedWriter, objects);	//write the objects
+			if(objects != null) { //if there are objects to serialize
+				CSVSerializer.serialize(bufferedWriter, objects); //write the objects
 			}
-			bufferedWriter.flush();	//flush everything we've written
-		}
-		finally
-		{
-			bufferedWriter.close();	//always close the stream
+			bufferedWriter.flush(); //flush everything we've written
+		} finally {
+			bufferedWriter.close(); //always close the stream
 		}
 	}
 }
--- trunk/globalmentor-core/src/main/java/com/globalmentor/text/csv/CSVSerializer.java
+++ trunk/globalmentor-core/src/main/java/com/globalmentor/text/csv/CSVSerializer.java
@@ -23,54 +23,48 @@ import static com.globalmentor.java.Characters.*;
 import static com.globalmentor.java.StringBuilders.*;
 import static com.globalmentor.text.csv.CSV.*;
 
-/**Serializes Comma Separated Value (CSV) information
-@author Garret Wilson
-@see <a href="http://www.rfc-editor.org/rfc/rfc4180.txt">RFC 4180: Common Format and MIME Type for Comma-Separated Values (CSV) Files</a>
-*/
-public class CSVSerializer
-{
-	/**Serializes the given objects to the provided writer.
-	@param writer The writer to which the objects will be serialized.
-	@param objects The objects to serialize, each of which may be <code>null</code>.
-	@throws IOException if there is an error writing the CSV record.
-	*/
-	public static void serialize(final Writer writer, final Object... objects) throws IOException
-	{
-		for(int i=0; i<objects.length; ++i)	//look at each object
-		{
-			if(i>0)	//if this isn't the first object
-			{
-				writer.write(FIELD_DELIMITER_CHAR);	//delimit the fields
+/**
+ * Serializes Comma Separated Value (CSV) information
+ * @author Garret Wilson
+ * @see <a href="http://www.rfc-editor.org/rfc/rfc4180.txt">RFC 4180: Common Format and MIME Type for Comma-Separated Values (CSV) Files</a>
+ */
+public class CSVSerializer {
+
+	/**
+	 * Serializes the given objects to the provided writer.
+	 * @param writer The writer to which the objects will be serialized.
+	 * @param objects The objects to serialize, each of which may be <code>null</code>.
+	 * @throws IOException if there is an error writing the CSV record.
+	 */
+	public static void serialize(final Writer writer, final Object... objects) throws IOException {
+		for(int i = 0; i < objects.length; ++i) { //look at each object
+			if(i > 0) { //if this isn't the first object
+				writer.write(FIELD_DELIMITER_CHAR); //delimit the fields
 			}
-			final Object object=objects[i];	//get a reference to this object
-			if(object!=null)	//if there is an object to write
-			{
-				final String field=encodeField(object.toString());	//get the string value of the object
-				writer.write(field);	//write the field
+			final Object object = objects[i]; //get a reference to this object
+			if(object != null) { //if there is an object to write
+				final String field = encodeField(object.toString()); //get the string value of the object
+				writer.write(field); //write the field
 			}
 		}
-		writer.write(RECORD_DELIMITER_STRING);	//write the end of the record
+		writer.write(RECORD_DELIMITER_STRING); //write the end of the record
 	}
 
-	/**Encodes a string to be represented in a CSV field.
-	If the character sequence includes a comma, double-quote, or a line break (CR or LF),
-	the string is encoded and placed in quotes. Double-quotes are escaped by doubling them.
-	@param charSequence The character sequence to encode. 
-	@return An encoded string appropriate for serialization as a CSV field.
+	/**
+	 * Encodes a string to be represented in a CSV field. If the character sequence includes a comma, double-quote, or a line break (CR or LF), the string is
+	 * encoded and placed in quotes. Double-quotes are escaped by doubling them.
+	 * @param charSequence The character sequence to encode.
+	 * @return An encoded string appropriate for serialization as a CSV field.
 	 */
-	public static String encodeField(final CharSequence charSequence)
-	{
-		if(charIndexOf(charSequence, RESTRICTED_CHARACTERS)>=0)	//if a restricted character is in the character sequence
-		{
-			final StringBuilder stringBuilder=new StringBuilder(charSequence);	//create a new string buffer to process the string 
-			replace(stringBuilder, QUOTATION_MARK_CHAR, ESCAPED_QUOTATION_MARK_STRING);	//replace all quotes with doubled quotes
-			stringBuilder.insert(0, QUOTATION_MARK_CHAR);	//prepend a quotation mark
-			stringBuilder.append(QUOTATION_MARK_CHAR);	//append a quotation mark
-			return stringBuilder.toString();	//return the string we created
-		}
-		else	//if there is no restricted character
-		{
-			return charSequence.toString();	//return the character sequence we started with
+	public static String encodeField(final CharSequence charSequence) {
+		if(charIndexOf(charSequence, RESTRICTED_CHARACTERS) >= 0) { //if a restricted character is in the character sequence
+			final StringBuilder stringBuilder = new StringBuilder(charSequence); //create a new string buffer to process the string 
+			replace(stringBuilder, QUOTATION_MARK_CHAR, ESCAPED_QUOTATION_MARK_STRING); //replace all quotes with doubled quotes
+			stringBuilder.insert(0, QUOTATION_MARK_CHAR); //prepend a quotation mark
+			stringBuilder.append(QUOTATION_MARK_CHAR); //append a quotation mark
+			return stringBuilder.toString(); //return the string we created
+		} else { //if there is no restricted character
+			return charSequence.toString(); //return the character sequence we started with
 		}
 	}
 }
